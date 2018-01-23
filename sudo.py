b1ecf405a3a56512e42f218b216223919c0b0ffa
Chronial <git@chronial.de>
2018-01-23 16:39:02 +0100

    Merge client and server into single file

--- sudo.py
+++ sudo.py
@@ -3,14 +3,17 @@ import argparse
 import errno
 import fcntl
 import os
+import pty
 import select
 import signal
 import socket
 import struct
 import subprocess
-import tempfile
 import sys
+import tempfile
+import traceback
 import tty
+from concurrent.futures import ThreadPoolExecutor
 from contextlib import ExitStack, closing, contextmanager
 
 import termios
@@ -19,81 +22,193 @@ CMD_DATA = 1
 CMD_WINSZ = 2
 
 
-def xselect(*args):
-    while True:
+class PartialRead(Exception):
+    pass
+
+
+class ElevatedServer:
+    def main(self, argv):
+        port = int(argv[1])
+        password_file = argv[2]
+        with open(password_file, 'rb') as f:
+            password = f.read()
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        with closing(self.sock):
+            self.sock.connect(('127.0.0.1', port))
+            received_password = self.recv_message()
+            if received_password != password:
+                print("ERROR: invalid password")
+                sys.exit(1)
+
+            child_args = [self.recv_message() for _ in range(4)]
+            print("Elevated sudo server running:")
+            print("> " + child_args[0].decode())
+
+            child_pid, child_pty = pty.fork()
+            if child_pid == 0:
+                self.child_process(*child_args)
+            else:
+                self.child_pid = child_pid
+                self.child_pty = child_pty
+                self.main_process()
+
+    def main_process(self):
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(self.pty_read_loop)
+            executor.submit(self.sock_read_loop)
+
+    def child_process(self, cmdline, cwd, winsize, env):
         try:
-            return select.select(*args)
-        except select.error as e:
-            if e.args[0] != errno.EINTR:
-                raise
+            self.sock.close()
+            os.chdir(cwd)
+            fcntl.ioctl(0, termios.TIOCSWINSZ, winsize)
+            envdict = dict(line.split(b'=', 1) for line in env.split(b'\0'))
+            envdict[b'ELEVATED_SHELL'] = b'1'
+            argv = cmdline.split(b'\0')
+            os.execvpe(argv[0], argv, envdict)
+        except BaseException:
+            traceback.print_exc()
+        finally:
+            sys._exit(1)
+
+    def recv_n(self, n):
+        d = []
+        while n > 0:
+            s = self.sock.recv(n)
+            if not s:
+                break
+            d.append(s)
+            n -= len(s)
+        if n > 0:
+            raise PartialRead('EOF while reading')
+        return b''.join(d)
+
+    def recv_message(self):
+        length = struct.unpack('I', self.recv_n(4))[0]
+        return self.recv_n(length)
+
+    def try_read(self, fd, size):
+        try:
+            return os.read(fd, size)
+        except OSError:
+            return b''
+
+    def pty_read_loop(self):
+        try:
+            for chunk in iter(lambda: self.try_read(self.child_pty, 8192), b''):
+                self.sock.sendall(chunk)
+            self.sock.shutdown(socket.SHUT_WR)
+        except Exception as e:
+            traceback.print_exc()
+
+    def sock_read_loop(self):
+        try:
+            while True:
+                message = self.recv_message()
+                id, data = struct.unpack('I', message[:4])[0], message[4:]
+                if id == CMD_DATA:
+                    os.write(self.child_pty, data)
+                elif id == CMD_WINSZ:
+                    fcntl.ioctl(self.child_pty, termios.TIOCSWINSZ, data)
+                    os.kill(self.child_pid, signal.SIGWINCH)
+        except PartialRead:
+            print('FIN received')
+        except Exception:
+            traceback.print_exc()
 
 
-def get_winsize():
-    if not os.isatty(0):
-        return struct.pack('HHHH', 24, 80, 640, 480)
+class UnprivilegedClient:
+    def main(self, command, window, **kwargs):
+        password = os.urandom(32)
+        with tempfile.NamedTemporaryFile("wb") as pwf:
+            pwf.write(password)
+            pwf.flush()
+            listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            listen_socket.bind(('127.0.0.1', 0))
+            with closing(listen_socket):
+                port = listen_socket.getsockname()[1]
+                listen_socket.listen(1)
 
-    winsz = struct.pack('HHHH', 0, 0, 0, 0)
-    return fcntl.ioctl(0, termios.TIOCGWINSZ, winsz)
+                subprocess.run(["cygstart", "--action=runas", window,
+                                sys.executable, __file__,
+                                '--elevated', 'server', str(port), pwf.name])
 
+                self.sock, acc = listen_socket.accept()
 
-@contextmanager
-def raw_term_mode():
-    if not os.isatty(0):
-        yield
-    else:
-        with ExitStack() as stack:
-            attr = termios.tcgetattr(0)
-            stack.callback(termios.tcsetattr, 0, termios.TCSAFLUSH, attr)
+            command_bytes = list(map(os.fsencode, command))
+            self.run(password, command_bytes)
 
-            def sighandler(n, f):
-                stack.close()
-                sys.exit(2)
+    def run(self, password, command):
+        with closing(self.sock):
+            self.send_message(password)
+            self.send_message(b'\0'.join(command))
+            self.send_message(os.fsencode(os.getcwd()))
+            self.send_message(self.get_winsize())
+            self.send_message(b'\0'.join(b'%s=%s' % t for t in os.environb.items()))
 
-            tty.setraw(0)
-            for sig in (signal.SIGINT, signal.SIGTERM):
-                signal.signal(sig, sighandler)
+            def handle_sigwinch(n, f):
+                self.send_command(CMD_WINSZ, self.get_winsize())
 
-            yield
+            signal.signal(signal.SIGWINCH, handle_sigwinch)
 
-
-def send_message(fd, data):
-    length = len(data)
-    fd.send(struct.pack('I', length))
-    fd.send(data)
-
-
-def send_command(fd, cmd, data):
-    send_message(fd, struct.pack('I', cmd) + data)
-
-
-def client_main(sock, password, command):
-    with closing(sock):
-        send_message(sock, password)
-        send_message(sock, b'\0'.join(command))
-        send_message(sock, os.fsencode(os.getcwd()))
-        send_message(sock, get_winsize())
-        send_message(sock, b'\0'.join(b'%s=%s' % t for t in os.environb.items()))
-
-        def handle_sigwinch(n, f):
-            send_command(sock, CMD_WINSZ, get_winsize())
-
-        signal.signal(signal.SIGWINCH, handle_sigwinch)
-
-        with raw_term_mode():
-            fdset = [0, sock.fileno()]
-            done = False
-            while not done:
-                for fd in xselect(fdset, (), ())[0]:
-                    if fd == 0:
-                        send_command(sock, CMD_DATA, os.read(0, 8192))
-                    else:
-                        data = sock.recv(8192)
-                        if data:
-                            os.write(1, data)
+            with self.raw_term_mode():
+                fdset = [0, self.sock.fileno()]
+                done = False
+                while not done:
+                    for fd in self.xselect(fdset, (), ())[0]:
+                        if fd == 0:
+                            self.send_command(CMD_DATA, os.read(0, 8192))
                         else:
-                            done = True
+                            data = self.sock.recv(8192)
+                            if data:
+                                os.write(1, data)
+                            else:
+                                done = True
 
-        sock.shutdown(socket.SHUT_WR)
+            self.sock.shutdown(socket.SHUT_WR)
+
+    def xselect(self, *args):
+        while True:
+            try:
+                return select.select(*args)
+            except select.error as e:
+                if e.args[0] != errno.EINTR:
+                    raise
+
+    @contextmanager
+    def raw_term_mode(self):
+        if not os.isatty(0):
+            yield
+        else:
+            with ExitStack() as stack:
+                attr = termios.tcgetattr(0)
+                stack.callback(termios.tcsetattr, 0, termios.TCSAFLUSH, attr)
+
+                def sighandler(n, f):
+                    stack.close()
+                    sys.exit(2)
+
+                tty.setraw(0)
+                for sig in (signal.SIGINT, signal.SIGTERM):
+                    signal.signal(sig, sighandler)
+
+                yield
+
+    def get_winsize(self):
+        if not os.isatty(0):
+            return struct.pack('HHHH', 24, 80, 640, 480)
+
+        winsz = struct.pack('HHHH', 0, 0, 0, 0)
+        return fcntl.ioctl(0, termios.TIOCGWINSZ, winsz)
+
+    def send_message(self, data):
+        length = len(data)
+        self.sock.send(struct.pack('I', length))
+        self.sock.send(data)
+
+    def send_command(self, cmd, data):
+        self.send_message(struct.pack('I', cmd) + data)
 
 
 def main():
@@ -110,24 +225,10 @@ def main():
     parser.add_argument('command', nargs=argparse.PARSER)
     args = parser.parse_args()
 
-    password = os.urandom(32)
-    sudoserver = os.path.dirname(os.path.abspath(__file__)) + '/sudoserver.py'
-    with tempfile.NamedTemporaryFile("wb") as pwf:
-        pwf.write(password)
-        pwf.flush()
-        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listen_socket.bind(('127.0.0.1', 0))
-        with closing(listen_socket):
-            port = listen_socket.getsockname()[1]
-            listen_socket.listen(1)
-
-            subprocess.run(["cygstart", "--action=runas", args.window,
-                            sys.executable, sudoserver, str(port), pwf.name])
-
-            connection, acc = listen_socket.accept()
-
-        command_bytes = list(map(os.fsencode, args.command))
-        client_main(connection, password, command_bytes)
+    if args.elevated:
+        ElevatedServer().main(args.command)
+    else:
+        UnprivilegedClient().main(**vars(args))
 
 
 if __name__ == '__main__':
